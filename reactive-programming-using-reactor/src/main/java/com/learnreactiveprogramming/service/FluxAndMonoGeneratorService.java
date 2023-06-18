ff09fee07cec156a9bce96ca5d238fd2ee31803a
Thitiwas Nupan <thitiwas.nn@gmail.com>
2023-06-18 16:12:23 +0700

    update

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,32 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+                .log(); // db or a remote service call
+    }
+
+    public Mono<String> namesMono() {
+        return Mono.just("alex")
+                .log();
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService service = new FluxAndMonoGeneratorService();
+        service.namesFlux()
+                .subscribe(s -> {
+                    System.out.println("Name is :" + s);
+                });
+
+        service.namesMono()
+                .subscribe(s -> {
+                    System.out.println("Name is :" + s);
+                });
+    }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,26 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
 
+    @Test
+    void nameFlux() {
+        // given
+
+        // when
+        var stringFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        // then
+        StepVerifier.create(stringFlux)
+//                .expectNext("alex", "ben", "chloe")
+                .expectNext("alex")
+                .expectNextCount(2)
+                .verifyComplete();
+
+    }
 }
